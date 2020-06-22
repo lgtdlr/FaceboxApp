3fbce8c09e33c1804553d2624e4dbaa1b9ef917e
Leonardo Torres <leonardo.torres5@upr.edu>
2020-06-21 23:22:30 -0400

    Added ImageSelect class & IdentifyActivity retrieves JSON

--- app/src/main/java/com/example/facebox/IdentifyActivity.java
+++ app/src/main/java/com/example/facebox/IdentifyActivity.java
@@ -2,17 +2,12 @@ package com.example.facebox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentUris;
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.database.Cursor;
-import android.media.Image;
+import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -34,10 +29,11 @@ public class IdentifyActivity extends AppCompatActivity {
     private static final String BASE_URL = "http://192.168.102.158:8080/facebox/check";
     private static final int PICK_IMAGE = 1;
 
-    private static ImageView selectedImage;
-    private static TextView requestResult, infoText;
+    ImageView selectedImage;
+    TextView requestResult, infoText;
+    ProgressDialog p;
 
-    private static OkHttpClient client = new OkHttpClient();
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,170 +63,74 @@ public class IdentifyActivity extends AppCompatActivity {
 
         if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
             Uri fullPhotoUri = data.getData();
-            final String fullPhotoPath = getPath(this, fullPhotoUri);
+            final String fullPhotoPath = ImageSelect.getPath(this, fullPhotoUri);
             selectedImage.setImageURI(fullPhotoUri);
 
             File file = new File(fullPhotoPath);
+            String fileName = file.getName();
 
-            if (uploadFile(BASE_URL, file)){
-                requestResult.setText("Uploaded");
-            } else {
-                return;
+            PostParameters params = new PostParameters(BASE_URL, file);
+            AsyncTaskExample asyncTask = new AsyncTaskExample();
+            if(asyncTask.execute(params).equals("Success")){
+                infoText.setText("Let's go");
             }
-
         }
     }
 
-    public Boolean uploadFile(String serverURL, File file) {
-        final Intent identifyIntent = new Intent(this, IdentifyActivity.class);
+    private class PostParameters {
+        String string;
+        File file;
 
-        try {
+        PostParameters(String string, File file) {
+            this.file = file;
+            this.string = string;
+        }
+    }
 
-            //error here...
-            RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
-                    .addFormDataPart("file", file.getName(), RequestBody.create(file, MediaType.get("image/jpeg")))
-                    .build();
-            Request request = new Request.Builder()
-                    .url(serverURL)
-                    .post(requestBody)
-                    .addHeader("Accept", "application/json; charset=utf-8")
-                    .build();
+    private class AsyncTaskExample extends AsyncTask<PostParameters, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            p = new ProgressDialog(IdentifyActivity.this);
+            p.setMessage("Please wait... Downloading");
+            p.setIndeterminate(false);
+            p.setCancelable(false);
+            p.show();
+        }
+        @Override
+        protected String doInBackground(PostParameters... params) {
+            try {
+                //error here...
+                RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                        .addFormDataPart("file", params[0].file.getName(), RequestBody.create(params[0].file, MediaType.get("image/jpeg")))
+                        .build();
+                Request request = new Request.Builder()
+                        .url(params[0].string)
+                        .post(requestBody)
+                        .addHeader("Accept", "application/json; charset=utf-8")
+                        .build();
 
-            client.newCall(request).enqueue(new Callback() {
-
-                @Override
-                public void onFailure(final Call call, final IOException e) {
-                    // Handle the error
-                    e.printStackTrace();
-                    startActivity(identifyIntent);
-                    return;
+                try (Response response = client.newCall(request).execute()) {
+                    return response.body().string();
                 }
 
-                @Override
-                public void onResponse(final Call call, final Response response) throws IOException {
-                    if (!response.isSuccessful()) {
-                        // Handle the error
-                        startActivity(identifyIntent);
-                    }
-                    // Upload successful
-                }
-            });
-
-            return true;
-        } catch (Exception ex) {
-            // Handle the error
-            ex.printStackTrace();
-        }
-        return false;
-    }
-
-    public static String getPath(final Context context, final Uri uri)
-    {
-
-        //check here to KITKAT or new version
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-
-        // DocumentProvider
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-
-            // ExternalStorageProvider
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-            }
-            // DownloadsProvider
-            else if (isDownloadsDocument(uri)) {
-
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://<span id=\"IL_AD1\" class=\"IL_AD\">downloads</span>/public_downloads"), Long.valueOf(id));
-
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[] {
-                        split[1]
-                };
-
-                return getDataColumn(context, contentUri, selection, selectionArgs);
+            } catch (Exception ex) {
+                // Handle the error
+                ex.printStackTrace();
+                return "Failure";
             }
         }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
 
-            // Return the remote address
-            if (isGooglePhotosUri(uri))
-                return uri.getLastPathSegment();
-
-            return getDataColumn(context, uri, null, null);
-        }
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
-        }
-
-        return null;
-    }
-
-    public static String getDataColumn(Context context, Uri uri, String selection,
-                                       String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {
-                column
-        };
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(index);
+        @Override
+        protected void onPostExecute(String string) {
+            super.onPostExecute(string);
+            if(selectedImage !=null) {
+                p.hide();
+                selectedImage.isOpaque();
+                infoText.setText(string);
+            }else {
+                p.show();
             }
-        } finally {
-            if (cursor != null)
-                cursor.close();
         }
-        return null;
-    }
-
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-
-    public static boolean isGooglePhotosUri(Uri uri) {
-        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 }
